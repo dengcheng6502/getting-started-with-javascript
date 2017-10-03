b9059ac2e9b8693d2a0d40d7b4bde41520df6d4c
dengcheng6502 <1332045221@qq.com>
2017-10-03 19:25:13 +0800

    diyiciwangye

--- homework/lesson6/dengcheng.js
+++ homework/lesson6/dengcheng.js
@@ -1,23 +1,26 @@
 //同步
 var fs =require('fs');
 var dirPathString='../../words';
-var files=fs.readdirSync(dirPathString);
+var files2=fs.readdirSync(dirPathString);
 var outputPathString = './limu.txt';
-fs.writeFileSync(outputPathString,files);
+fs.writeFileSync(outputPathString,files2);
 //异步
 var fs = require('fs');
 var readPathString = '../../words';
 var writePathString = './limu.txt';
 function read_callback(err,files){
-    if(err){console.log('读取文件失败');
-    return;
-}
-if(files.length>0){
-    var filescontent=files;
-    fs.writeFile(writePathString, fileContent, write_callback);}
+    if(err){
+        console.log('读取文件失败');
+        return;
+    }
+    
+    if(files.length>0){
+       var fileContent=files;
+       fs.writeFile(writePathString, fileContent, write_callback);
+    }
     else{
-        console.log('没有找到文件');}
-
+        console.log('没有找到文件');
+    }
 }
 
 function write_callback(err){
@@ -26,4 +29,4 @@ console.log('写文件失败');}
 else{console.log('写文件成功');
 }
 }
-fs.readdir(readPathString,writePathString)
+fs.readdir(readPathString,read_callback)
